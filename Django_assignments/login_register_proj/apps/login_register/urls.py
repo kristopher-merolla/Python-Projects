17760d7f3718599f6480ad5342a0fbbcfef9a4f5
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-05-25 12:07:36 -0700

    Added named routes for url.py file routes

--- Django_assignments/login_register_proj/apps/login_register/urls.py
+++ Django_assignments/login_register_proj/apps/login_register/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index),
-	url(r'^register$', views.register),
-	url(r'^login$', views.login),
-	url(r'^register_user$', views.new_login),
+	url(r'^$', views.index, name="index"),
+	url(r'^register$', views.register, name="register"),
+	url(r'^login$', views.login, name="login"),
+	url(r'^register_user$', views.new_login, name="register_user"),
 ]
